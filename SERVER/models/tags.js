bb61dffe80f8ecb8a7696d4731975f8cbe512b12
Sauravbanty07 <msdsauravbanty07@gmail.com>
2024-04-03 03:19:23 +0530

    mega bd 1 complete

---
+++ SERVER/models/tags.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+const tagsSchema = new mongoose.Schema({
+    
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+        // white spaces will be removed from both sides of the string in trim:true 
+    },
+    course:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Course"
+    }
+
+});
+
+module.exports= mongoose.model("Tag",tagsSchema)
+
+
